f3137cace7eedc36d4b37e97b9a822f4d6fd8758
Dangerswede <markus.arrhen@gmail.com>
2024-06-16 09:37:22 +0200

    Added charactercounter

--- main.py
+++ main.py
@@ -3,9 +3,8 @@ def main():
     text = get_book_text(book_path)
     num_words = get_num_words(text)
     num_characters = get_num_characters(text)
-    print(f"{num_words} words found in the document")
-    for char in sorted(num_characters):
-        print(f"'{char}': {num_characters[char]} characters found in document")
+    print_report(num_words, num_characters)
+    
 
 def get_lowered_string(text):
     lowered_text = text.lower()
@@ -29,5 +28,16 @@ def get_book_text(path):
     with open(path) as f:
         return f.read()
 
+def print_report(num_words, num_characters):
+    print("--- Begin report of books/frankenstein.txt ---")
+    print(f"{num_words} words found in the document")
+    
+    
+    character_list = list(num_characters.items())
+    character_list.sort(key=lambda item: item[1], reverse=True)
 
+    for char, count in character_list:
+        if char.isalpha() == True:
+            print(f"the '{char}'  character was found {count} times")
+    print("--- End report ---")
 main()
